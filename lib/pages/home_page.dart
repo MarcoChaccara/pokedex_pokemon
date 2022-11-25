78ed9275b845076dd4c424cb1aedae0c573ea6ec
Marco <201912953f@utea.edu.pe>
2022-11-24 23:27:12 -0500

    commit por si me duermo

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,29 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:pokemon/db/db_admin.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  
+  Future<String> getFullName() async{
+    return "Juan Manuel";
+  }
 
   @override
   Widget build(BuildContext context) {
+
+
+    DBAdmin.db.getTasks();
+
     return Scaffold(
       appBar: AppBar(
         title: Text("HomePage"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                DBAdmin.db.initDatabase();
-              },
-              child: Text(
-                "Mostrar data"
-              ),
-            ),
-          ],
-        ),
-      ),
+      // body: FutureBuilder(
+      //   future: DBAdmin.db.getTasks(),
+      //   builder:(BuildContext context, AsyncSnapshot snap){
+      //     if(snap.hasData){
+      //       List<Map<String, dynamic>> myTasks = snap.data;
+      //       return ListView.builder(
+      //         itemCount: myTasks.length,
+      //         itemBuilder: (BuildContext context, int index){
+      //           return ListTile(
+      //             title: Text(myTasks[index]["title"]),
+      //             subtitle: Text(myTasks[index]["description"]),
+      //             trailing: Text(myTasks[index]["id"].toString()),
+      //           );
+      //         },
+      //       );
+      //     }
+      //     return const Center(
+      //         child: CircularProgressIndicator(),
+      //     );
+      //   },
+      // ),
+      body: Center()
     );
   }
 }
