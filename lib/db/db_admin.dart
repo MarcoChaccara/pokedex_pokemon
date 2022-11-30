3cff3b214d7c60986f37e63a0cbc09f31e169c52
Marco <201912953f@utea.edu.pe>
2022-11-29 22:06:51 -0500

    Creación del formulario

--- lib/db/db_admin.dart
+++ lib/db/db_admin.dart
@@ -4,8 +4,9 @@ import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
-class DBAdmin{
-  
+import '../models/task_model.dart';
+
+class DBAdmin {
   Database? myDatabase;
 
   //Singleton
@@ -13,28 +14,94 @@ class DBAdmin{
   DBAdmin._();
   //
 
-  checkDatabase(){
-    if(myDatabase != null){
+  Future<Database?> checkDatabase() async {
+    if (myDatabase != null) {
       return myDatabase;
     }
-    myDatabase = initDatabase();// Crear base de datos
+    myDatabase = await initDatabase(); // Crear base de datos
     return myDatabase;
   }
 
-  initDatabase()async{
+  Future<Database> initDatabase() async {
     Directory directory = await getApplicationDocumentsDirectory();
     String path = join(directory.path, "TaskDB.db");
-    return await openDatabase(
-      path,
-      version: 1,
-      onOpen: (db){},
+    return await openDatabase(path, version: 1, onOpen: (db) {},
       onCreate: (Database dbx, int version) async{
-        //CREAR LA TABLA CORRESPONDIENTE
-        await dbx.execute("CREATE TABLE TASK(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, description TEXT, status TEXT)");
+      //CREAR LA TABLA CORRESPONDIENTE
+      await dbx.execute(
+        "CREATE TABLE TASK(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, description TEXT, status TEXT)");
       }
     );
   }
 
+  Future<int> insertRawTask(TaskModel model) async {
+    Database? db = await checkDatabase();
+    int res = await db!.rawInsert(
+    "INSERT INTO TASK(title,description, status) VALUES('${model.title}','${model.description}','${model.status.toString()}')");
+    return res;
+  }
 
+  Future<int> insertTask(TaskModel model) async {
+    Database? db = await checkDatabase();
+    int res = await db!.insert(
+      "TASK",
+      {
+        "title": model.title,
+        "description": model.description,
+        "status": model.status.toString(),
+      },
+    );
+    return res;
+  }
+  getRawTasks() async{
+    Database? db = await checkDatabase();
+    List tasks = await db!.rawQuery("SELECT * FROM Task");
+    print(tasks[0]);
+  }
+
+  Future<List<TaskModel>> getTasks() async{
+    Database? db = await checkDatabase();
+    List<Map<String, dynamic>> tasks = await db!.query("Task");
+    List<TaskModel> taskModelList = tasks.map((e) => TaskModel.deMapAModel(e)).toList();
+
+    // tasks.forEach((element){
+    //   TaskModel task = TaskModel.deMapAModel(element);
+    //   taskModelList.add(task);
+    // });
+
+    
+    return taskModelList;
+  }
+
+  updateRawTask() async{
+    Database? db = await checkDatabase();
+    int res = await db!.rawUpdate("UPDATE TASK SET title = 'Ir de compras', description = 'Comprar comida', status = 'true' where id = 2");
+    print(res);
+  }
+
+  updateTask() async{
+    Database? db = await checkDatabase();
+    db!.update(
+      "TASK",
+      {
+        "title": "Ir al cine",
+        "description": "Es el viernes en la tarde",
+        "status": "false",
+      },
+      where: "id = 2"
+    );
+  }
+
+  deleteRawTask() async{
+    Database? db = await checkDatabase();
+    int res = await db!.rawDelete("DELETE FROM TASK WHERE id = 2");
+    print(res);
+  }
+
+  deleteTask() async{
+    Database? db = await checkDatabase();
+    int res = await db!.delete("TASK", where: "id = 3");
+    print(res);
+  }
 
 }
